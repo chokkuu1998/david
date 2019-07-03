6b7947d65f0efb5c475c55a7dbb12f39e17cd299
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-03 16:18:14 +0530

    Create a5.py

---
+++ a5.py
@@ -0,0 +1,16 @@
+nn=int(input())
+ss=0
+for i in range(2,nn):
+    flag=1
+    for j  in range(2,i):
+        if(i%j==0):
+            flag=0
+            break
+    if(flag):
+        k=str(i)
+        l=[]
+        for m in range(len(k)):
+            l.append(k[m])
+        if(l[-1]=='3'):
+            s=s+i
+print(s)            
