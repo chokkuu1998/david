ddfc032ac90be88447ad39a0d3e17ff02b7c0282
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-12 13:49:43 +0530

    Create david4.py

---
+++ david4.py
@@ -0,0 +1,13 @@
+import sys, string, math
+a1 = input()
+b2 = input()
+c3 = string.ascii_lowercase
+Ll = []
+for i in range(0, len(a1)) :
+    k = ord(s1[i]) + sc.index(b2[i]) + 1
+    if k > ord('a')+25 :
+        k -= 26
+    #print(k,chr(k))
+    L.append(chr(k))
+s4 = ''.join(L)
+print(s4)
