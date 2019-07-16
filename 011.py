0f8bb6b1da02eadd0dcbc1f7dcc136cd6d61868c
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 12:24:32 +0530

    Update 011.py

--- 011.py
+++ 011.py
@@ -1,10 +1,10 @@
-st1=input()
-b=0
-c=[]
-for i in range(0,len(st1)-1):
-    for j in range(i+1,len(st1)):
-        k=st1[i:j+1]
+stt1=input()
+bb=0
+cc=[]
+for i in range(0,len(stt1)-1):
+    for j in range(i+1,len(stt1)):
+        k=stt1[i:j+1]
         l=k[::-1]
         if k==l:
-            c.append(len(st1)-len(l))
-print(min(c))
+            c.append(len(stt1)-len(l))
+print(min(cc))
