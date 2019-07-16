8c64d75cf3c361635fae257d9042ee193219c4e8
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 12:09:15 +0530

    Update 05.py

--- 05.py
+++ 05.py
@@ -1,9 +1,9 @@
-nn,kk = input().split()
-nn,kk = int(nn),int(kk)
-L = [ int(x) for x in input().split()]
-#print(nn,kk, L)
-for i in range(0,nn) :
-    if (86400-L[i]) >= kn :
-        print(i+1)
-        sys.exit()
-    kk -= (86400-L[i])
+nn,tt=map(int,input().split())
+aa=list(map(int,input().split()))
+dd=0
+ii=0
+while tt>0:
+    tt=tt-86400+aa[ii];
+    ii=ii+1
+    dd=dd+1
+print(dd)
