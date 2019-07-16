62300fc7b2dd6a5f25f76b4762540fa9896801bf
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 12:40:23 +0530

    Update 016.py

--- 016.py
+++ 016.py
@@ -1,19 +1,19 @@
-aa, bb, ff, kk = map(int, input().split())
-count = 0
-d = bb-ff
+a, b, f, k = map(int, input().split())
+c = 0
+d = b-f
 if (d >= 0):
-    s = (aa-ff)*2
+    s = (a-f)*2
     for i in range (k):
         if (i == k-1):
              s =s/ 2
         if (d < s):
-            d= bb
-            count += 1
+            d= b
+            c += 1
         d = d - s
         if (d < 0):
-            count = -1
+            c = -1
             break
-        s = 2*aa - s
-    print (count)
+        s = 2*a - s
+    print (c)
 else:
     print (-1)
