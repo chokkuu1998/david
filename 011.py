214b737b647485b4ab697e827e78d31dc560494f
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 12:26:12 +0530

    Update 011.py

--- 011.py
+++ 011.py
@@ -1,10 +1,12 @@
-stt1=input()
-bb=0
-cc=[]
-for i in range(0,len(stt1)-1):
-    for j in range(i+1,len(stt1)):
-        k=stt1[i:j+1]
-        l=k[::-1]
-        if k==l:
-            c.append(len(stt1)-len(l))
-print(min(cc))
+ss=str(input())
+ll=[]
+tt=""
+rr=0
+for i in range(0,len(ss)):
+    for j in range(i,len(ss)):
+        tt=tt+ss[j]
+        if(tt[::-1]==tt):
+            rr=len(ss)-len(tt)
+            ll.append(rr)
+    tt=""
+print(min(ll))
