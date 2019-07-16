d70ceeb6c00a15ba2a0b4f312242eb1d63c9b2ab
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 12:30:04 +0530

    Update 012.py

--- 012.py
+++ 012.py
@@ -1,17 +1,8 @@
-ss=str(input())
-tt=""
-rr=""
-ll=[]
-if(ss=="abcabcdddd"):
-    print(3)
-    exit(0)
-for i in range(0,len(ss)):
-    for j in range(i,len(ss)):
-        rr=tt
-        tt=tt+ss[j]
-        if ss[j] not in rr:
-            l.append(len(tt))
-        else:
-            break
-    tt=""
-print(max(ll))
+inpp=input()
+dumm=[]
+for i in inp:
+  if i not in dumm:
+    dumm.append(i)
+  else:
+    break  
+print(len(dumm))
