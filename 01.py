9683bb35d881fbe4cdaf88bcde1f5dc25f822395
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 11:56:38 +0530

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,8 @@
+xx1,yy1=map(int,input().split())
+xx2,yy2=map(int,input().split())
+xx3,yy3=map(int,input().split())
+xx4,yy4=map(int,input().split())
+if(abs(xx1-xx2)==(xx4-xx3))==abs((yy1-y4)==(yy2-y3)):
+    print("yes")
+else:
+    print("no")
